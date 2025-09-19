06c6d0273bd6a6384918c3e096ab9b329dd12796
houzhenming <hou.zaming@gmail.com>
2025-09-19 14:26:20 +0800

    -Better GUI

--- src/main/java/gui/DialogBox.java
+++ src/main/java/gui/DialogBox.java
@@ -1,31 +1,60 @@
 package gui;
 
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.image.Image;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class DialogBox extends HBox {
 
-    private Label text;
-    private ImageView displayPicture;
+    private final Label bubble;
+    private final ImageView avatar;
 
-    public DialogBox(String s, ImageView i) {
-        text = new Label(s);
-        displayPicture = i;
+    private static final double AVATAR_SIZE = 36;
+    private static final double MAX_BUBBLE_WIDTH = 420;
 
-        text.setWrapText(true);
-        displayPicture.setFitWidth(100.0);
-        displayPicture.setFitHeight(100.0);
+    private DialogBox(String text, ImageView imageView) {
         this.setAlignment(Pos.TOP_RIGHT);
+        this.setSpacing(10);
+        this.setPadding(new Insets(6, 12, 6, 12));
 
-        this.getChildren().addAll(text, displayPicture);
+        // Avatar (circular clip like Telegram)
+        this.avatar = imageView;
+        this.avatar.setFitWidth(AVATAR_SIZE);
+        this.avatar.setFitHeight(AVATAR_SIZE);
+        Circle clip = new Circle(AVATAR_SIZE / 2.0, AVATAR_SIZE / 2.0, AVATAR_SIZE / 2.0);
+        this.avatar.setClip(clip);
+
+        // Bubble
+        this.bubble = new Label(text);
+        this.bubble.setWrapText(true);
+        this.bubble.setMaxWidth(MAX_BUBBLE_WIDTH);
+        this.bubble.setPadding(new Insets(10, 14, 10, 14));
+        this.bubble.setStyle(
+                "-fx-background-color: #d2e3ff;" +           // default user color
+                        "-fx-text-fill: -fx-text-base-color;" +
+                        "-fx-background-radius: 16 16 4 16;" +       // one corner sharper (tail side)
+                        "-fx-font-size: 13px;" +
+                        "-fx-line-spacing: 2px;"
+        );
+
+        DropShadow ds = new DropShadow();
+        ds.setRadius(4);
+        ds.setOffsetY(1.5);
+        ds.setColor(Color.rgb(0, 0, 0, 0.12));
+        this.bubble.setEffect(ds);
+
+        this.getChildren().addAll(bubble, avatar);
     }
 
+    /** Flip alignment + order for "received" messages (Duke). */
     private void flip() {
         this.setAlignment(Pos.TOP_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
@@ -33,14 +62,41 @@ public class DialogBox extends HBox {
         this.getChildren().setAll(tmp);
     }
 
-    public static DialogBox getUserDialog(String s, ImageView i) {
-        return new DialogBox(s, i);
+    /** Tweak bubble style for sender (user). */
+    private void styleAsUser() {
+        // Blue-ish bubble, tail on bottom-right
+        this.bubble.setStyle(
+                "-fx-background-color: #d2e3ff;" +
+                        "-fx-text-fill: -fx-text-base-color;" +
+                        "-fx-background-radius: 16 16 4 16;" +
+                        "-fx-font-size: 13px;" +
+                        "-fx-line-spacing: 2px;"
+        );
     }
 
-    public static DialogBox getDukeDialog(String s, ImageView i) {
-        var db = new DialogBox(s, i);
-        db.flip();
+    /** Tweak bubble style for receiver (duke/bot). */
+    private void styleAsDuke() {
+        // Neutral grey bubble, tail on bottom-left
+        this.bubble.setStyle(
+                "-fx-background-color: #f1f3f4;" +
+                        "-fx-text-fill: -fx-text-base-color;" +
+                        "-fx-background-radius: 16 4 16 16;" +
+                        "-fx-font-size: 13px;" +
+                        "-fx-line-spacing: 2px;"
+        );
+    }
+
+    public static DialogBox getUserDialog(String text, ImageView img) {
+        var db = new DialogBox(text, img);
+        db.styleAsUser();
+        // Right-aligned order: [bubble][avatar]
         return db;
     }
 
+    public static DialogBox getDukeDialog(String text, ImageView img) {
+        var db = new DialogBox(text, img);
+        db.styleAsDuke();
+        db.flip(); // Left-aligned order: [avatar][bubble]
+        return db;
+    }
 }
